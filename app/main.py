954a03a1e6acf15f7c1d5da411cf80db0a89596e
Guilherme <guilherme.prado@lnls.br>
2017-10-09 16:07:01 -0300

    First version of the api Flask server

--- app/main.py
+++ app/main.py
@@ -10,24 +10,17 @@ Endpoints:
     Get all Values of a PvConfiguration
     Insert new Value for PvConfiguration
 """
+import json
 from flask import Flask, request, jsonify
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm.exc import NoResultFound
+from flask_pymongo import PyMongo, ObjectId
 
-from models import Base, PvConfiguration, PvConfigurationValue
 
 app = Flask(__name__)
 
-engine = create_engine(
-    "mysql+pymysql://root:root@config-db/config_db")
-Base.metadata.bind = engine
-# Base.metadata.create_all(engine)
+app.config["MONGO_DBNAME"] = "config_database"
+app.config["MONGO_URI"] = "mongodb://config-db:27017/config_database"
 
-db_session = sessionmaker(bind=engine)
-session = db_session()
-
+mongo = PyMongo(app)
 
 # Taken from Flask site
 class InvalidUsage(Exception):
@@ -58,183 +51,157 @@ def handle_invalid_usage(error):
     return response
 
 
+def check_pv_collection(collection):
+    """Check pv_configuration docuements."""
+    for document in collection:
+        if not check_pv_document(document):
+            return False
+    return True
+
+
+def check_values_collection(collection):
+    """Check pv_configuration_value docuements."""
+    for document in collection:
+        if not check_values_document(document):
+            return False
+    return True
+
+
+def check_pv_document(document):
+    """Check if document has the required fields."""
+    if "name" not in document:
+        print("name not found")
+        return False
+    if "config_type" not in document:
+        print("config_type not found")
+        return False
+    if "values" not in document:
+        print("values not found")
+        return False
+    return check_values_collection(document["values"])
+
+
+def check_values_document(document):
+    """Check if embedded document has the required fields."""
+    if "pv_name" not in document:
+        print("pvname not found")
+        return False
+    if "pv_type" not in document:
+        print("pvtype not found")
+        return False
+    if "value" not in document:
+        print("value not found")
+        return False
+    return True
+
+
 # Endpoints
 @app.route("/")
 @app.route("/configs", methods=["GET", "POST"])
 def pvConfigurations():
     """Endpoint to get and insert configuration."""
     if request.method == "GET":
-        return getAllPvConfigurations()
+        data = request.get_json()
+        if data is None:
+            return getAllPvConfigurations()
+        else:
+            return queryPvConfiguration(data)
     elif request.method == "POST":
+        data = request.get_json()
         # Insert new configuration
-        name = request.args.get("name")
-        config_type = request.args.get("config_type")
-        return makeNewPvConfiguration(name, config_type)
+        return makeNewPvConfiguration(data)
 
 
-@app.route("/configs/<int:id>", methods=["GET", "PUT", "DELETE"])
+@app.route("/configs/<string:id>", methods=["GET", "PUT", "DELETE"])
 def pvConfigurationsId(id):
     """Endpoint to get, update or delete a pv configuration by the id."""
+    id = ObjectId(id)
     if request.method == "GET":
-        return getPvConfiguration(id)
+        return queryPvConfiguration({"_id": id})
     elif request.method == "PUT":
-        name = request.args.get("name", None)
-        config_type = request.args.get("config_type", None)
-        return updatePvConfiguration(id, name, config_type)
+        data = request.get_json()
+        if data is None:
+            raise InvalidUsage("Update parameters missing", status_code=400)
+        return updatePvConfiguration(id, data)
     elif request.method == "DELETE":
         return deletePvConfiguration(id)
-
-
-@app.route("/configs/<int:config_id>/values", methods=["GET", "POST"])
-def pvConfigurationsValues(config_id):
-    """Endpoint that return all values of configuration."""
-    if request.method == "GET":
-        return getAllPvConfigurationValues(config_id)
-    if request.method == "POST":
-        pv_name = request.args.get("pv_name")
-        pv_type = request.args.get("pv_type")
-        value = request.args.get("value")
-        return makeNewPvConfigurationValue(pv_name, pv_type, value, config_id)
-
-
-@app.route("/values/<int:id>", methods=["GET", "PUT", "DELETE"])
-def pvConfigurationsValuesId(id):
-    """Endpoint to get, update or delete a pv configuration value by the id."""
-    if request.method == "GET":
-        return getPvConfigurationValues(id)
-    elif request.method == "PUT":
-        pv_name = request.args.get("pv_name", None)
-        pv_type = request.args.get("pv_type", None)
-        value = request.args.get("value", None)
-        return updatePvConfigurationValue(id, pv_name, pv_type, value)
-    elif request.method == "DELETE":
-        return deletePvConfigurationValue(id)
 
 
 # PvConfiguration DB access functions
 def getAllPvConfigurations():
     """Return all pv configurations."""
-    configs = session.query(PvConfiguration).all()
-    return jsonify(PvConfiguration=[i.serialize for i in configs])
+    output = []
+    for pv_configuration in mongo.db.pv_configuration.find():
+        output.append(serialize(pv_configuration))
+    return jsonify(result=output)
 
 
-def makeNewPvConfiguration(name, config_type):
+def makeNewPvConfiguration(data):
     """Insert new Pv configuration."""
-    config = PvConfiguration(name=name, config_type=config_type)
-    session.add(config)
-    commit()
-    return jsonify(PvConfiguration=config.serialize)
+    if type(data) is dict:
+        # Sanity check
+        if not check_pv_document(data):
+            raise InvalidUsage("Invalid document", status_code=400)
+        # Insert
+        try:
+            result = mongo.db.pv_configuration.insert_one(data)
+        except Exception as e:  # Change to specific exception
+            raise InvalidUsage("Duplicate Key", status_code=409)
+        else:
+            return jsonify(result=str(result.inserted_id))
+    elif type(data) is list:  # Bulk write
+        # raise InvalidUsage("Bulk write not allowed", status_code=400)
+        if not check_pv_collection(data):
+            raise InvalidUsage("Invalid document", status_code=400)
+        try:
+            result = mongo.db.pv_configuration.insert_many(data)
+        except Exception as e:
+            raise InvalidUsage("Error", status_code=409)
+        else:
+            return jsonify(result=[str(id) for id in result.inserted_ids])
 
 
-def getPvConfiguration(id):
-    """Return pv configuration."""
-    config = getPvConfigurationById(id)
-    return jsonify(PvConfiguration=config.serialize)
+def queryPvConfiguration(data):
+    """Return a pv configuration based on data."""
+    output = []
+    results = mongo.db.pv_configuration.find(data)
+    for pv_configuration in results:
+        output.append(serialize(pv_configuration))
+    return jsonify(result=output)
 
 
-def updatePvConfiguration(id, name, config_type):
-    """Update pv configuration."""
-    config = getPvConfigurationById(id)
-    if name is not None:
-        config.name = name
-    if config_type is not None:
-        config.config_type = config_type
-    session.add(config)
-    commit()
-    return jsonify(PvConfiguration=config.serialize)
+def updatePvConfiguration(id, data):
+    """Update a pv configuration."""
+    try:
+        result = mongo.db.pv_configuration.update_one(
+            {"_id": id}, {"$set": data})
+    except Exception as e:
+        raise InvalidUsage("Duplicate Key?", status_code=409)
+    else:
+        return jsonify(result=result.modified_count)
 
 
 def deletePvConfiguration(id):
-    """Delete PV configuration."""
-    config = getPvConfigurationById(id)
-    session.delete(config)
-    commit()
-    return jsonify(PvConfiguration=config.serialize)
-
-
-def getAllPvConfigurationValues(config_id):
-    """Get all values of a configuration."""
-    # values = session.query(PvConfigurationValue).filter_by(
-    #     pv_configuration_id=config_id)
-    config = getPvConfigurationById(config_id)
-    values = session.query(PvConfigurationValue).filter_by(
-        pv_configuration=config)
-    return jsonify(PvConfigurationValue=[i.serialize for i in values])
-
-
-def makeNewPvConfigurationValue(pv_name, pv_type, value, config_id):
-    """Insert new value to a pv configuration."""
-    # Create new config value
-    value = PvConfigurationValue(pv_name=pv_name, pv_type=pv_type, value=value)
-    config = getPvConfigurationById(config_id)
-    value.pv_configuration = config
-    session.add(value)
-    commit()
-    return jsonify(PvConfigurationValue=value.serialize)
-
-
-def getPvConfigurationValues(id):
-    """Return Pv configuration value."""
-    value = getPvConfigurationValueById(id)
-    return jsonify(PvConfigurationValue=value.serialize)
-
-
-def updatePvConfigurationValue(id, pv_name, pv_type, value):
-    """Update a pv configuration value."""
-    config_value = getPvConfigurationValueById(id)
-    if pv_name is not None:
-        config_value.pv_name = pv_name
-    if pv_type is not None:
-        config_value.pv_type = pv_type
-    if value is not None:
-        config_value.value = value
-    session.add(config_value)
-    commit()
-    return jsonify(PvConfigurationValue=config_value.serialize)
-
-
-def deletePvConfigurationValue(id):
-    """Delete a pv configuration value."""
-    config_value = getPvConfigurationValueById(id)
-    session.delete(config_value)
-    commit()
-    return jsonify(PvConfigurationValue=config_value.serialize)
+    """Delete a PV configuration."""
+    try:
+        result = mongo.db.pv_configuration.delete_one({"_id": id})
+    except Exception as e:
+        raise InvalidUsage("{}".format(e), status_code=400)
+    else:
+        return jsonify(result=result.deleted_count)
 
 
 # Helpers
-def getPvConfigurationById(id):
-    """Retrieve PvConfiguration from DB."""
-    try:
-        config = session.query(PvConfiguration).filter_by(id=id).one()
-    except NoResultFound:
-        # Not Found
-        raise InvalidUsage("PvConfiguration not found.", status_code=404)
-    return config
+def serialize(document):
+    """Serialize pv_configuration dict."""
+    return {
+        "_id": str(document["_id"]),
+        "name": document["name"],
+        "config_type": document["config_type"],
+        "values": document["values"]
+    }
 
-
-def getPvConfigurationValueById(id):
-    """Retrieve PvConfigurationValue from DB."""
-    try:
-        value = session.query(PvConfigurationValue).filter_by(id=id).one()
-    except NoResultFound:
-        # Not found
-        raise InvalidUsage("PvConfigurationValue not found", status_code=404)
-    return value
-
-
-def commit():
-    """Commit DB transactions. Rollback if anything goes wrong."""
-    try:
-        session.commit()
-    except IntegrityError as e:
-        session.rollback()
-        # The 409 (Conflict) status code indicates that the request could not
-        # be completed due to a conflict with the current state of the target
-        # resource.  This code is used in situations where the user might be
-        # able to resolve the conflict and resubmit the request. (RFC 7231)
-        raise InvalidUsage("{}".format(e.orig), status_code=409)
-
-
+    
 if __name__ == "__main__":
     app.debug = True
     app.run(host="0.0.0.0", port=5000)
