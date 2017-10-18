f371ad30d82e1ada4b644a420be9f3c7b6224653
Guilherme <guilherme.prado@lnls.br>
2017-10-18 15:03:53 -0200

    Updated lists index

--- db_configuration.js
+++ db_configuration.js
@@ -3,4 +3,4 @@ db = conn.getDB("config_database");
 db.pv_configuration.createIndex({name: 1, config_type: 1}, { unique: true});
 db.pv_configuration.createIndex({name: 1, config_type: 1, "values.pv_name": 1}, {unique: true});
 
-db.lists.createIndex({name:1}, {unique: true})
+db.lists.createIndex({name: 1, config_type: 1}, {unique: true})
