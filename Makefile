5ce0514d6c22aa1212d985cfaebcac3c3e8e4b56
ximenes <xresende@gmail.com>
2017-11-14 10:58:48 -0200

    Update backup and install files

---
+++ Makefile
@@ -0,0 +1,9 @@
+INSTALL_DIR=/usr/local/bin
+
+install:
+	cp -f scripts/* $(INSTALL_DIR)/
+	$(MAKE) -C systemd
+
+develop:
+	sudo ln -fs $(PWD)/scripts/* $(INTALL_DIR)/
+	$(MAKE) -C systemd
--- scripts/sirius-configdb-backup.sh
+++ scripts/sirius-configdb-backup.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-WRK_DIR="/tmp"
-BKP_DIR="/home/fac_files/lnls-sirius/docker-config-db/backups"
+
+BKP_DIR=$ROOT_GROUP/backups/configdb
+WRK_DIR=/tmp
 CONTAINER_NAME="docker_config-db_1"
 NETWORK="docker_default"
+
 function backup_databases {
     RUNNING=$(sudo docker ps | grep docker_config-db_1)
     if [ ! -z "${RUNNING}" ]; then
 	      printf "backing dockerconfigdb_config-db_1 up ...\n"
         fname=dump_config_db_`date +%Y-%m-%d"_"%H-%M-%S`
-        mkdir -p $WRK_DIR/backup
-        chmod +777 $WRK_DIR/backup/
+        mkdir -p $WRK_DIR/$fname/
+        chmod +777 $WRK_DIR/$fname/
         docker run --rm --network $NETWORK --link $CONTAINER_NAME:mongo -v $WRK_DIR/backup:/backup mongo mongodump --out=/backup --host=$CONTAINER_NAME
-        tar zcvf $BKP_DIR/$fname.tar.gz $WRK_DIR/backup/
-        rm -rf $WRK_DIR/backup
+        tar zcvf $BKP_DIR/$fname.tar.gz $WRK_DIR/$fname
+        rm -rf $WRK_DIR/$fname
     else
         printf "service docker_config-db_1 not running\n"
     fi
