1a9123ba76269bff811571ba4595d7791eab5008
Guilherme <guilherme.prado@lnls.br>
2018-01-05 09:20:53 -0200

    Set backup folder path

--- scripts/sirius-configdb-restore.sh
+++ scripts/sirius-configdb-restore.sh
@@ -4,10 +4,10 @@ BKP_FOLDER=$(echo $BKP_FILE | sed 's/\..*//')
 CONTAINER_NAME="config-db"
 NETWORK="config-service"
 WRK_DIR=/tmp/$CONTAINER_NAME
-BKP_DIR=$ROOT_GROUP/backups/configdb"
+BKP_DIR=/home/fac_files/backups/configdb
 
 mkdir -p $WRK_DIR
 tar -C $WRK_DIR -xvf $BKP_DIR/$1
 chmod -R +777 $BKP_DIR
-sudo docker run --rm --network $NETWORK --link $CONTAINER_NAME:mongo -v $WRK_DIR/$BKP_FOLDER:/backup mongo mongorestore --drop -v --host $CONTAINER_NAME /backup
+sudo docker run --rm --network $NETWORK --link $CONTAINER_NAME:mongo -v $WRK_DIR/tmp/$BKP_FOLDER:/backup mongo mongorestore --drop -v --host $CONTAINER_NAME /backup
 rm -rf $WRK_DIR
