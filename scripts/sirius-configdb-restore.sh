0528afd04b00aca29682c5ad534c11372b3bc283
Guilherme <guilherme.prado@lnls.br>
2018-09-21 16:06:33 -0300

    Added parameters to restore script

--- scripts/sirius-configdb-restore.sh
+++ scripts/sirius-configdb-restore.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
+
+if [[ $# -ne 3 ]]; then
+	echo 'Usage: backup_file container_name container_network'
+	echo ''
+
+	echo 'Containers:'
+	echo $(docker container ls --format "{{.Names}}")
+	echo ''
+
+	echo 'Networks:'
+	echo $(docker network ls --format "{{.Name}}")
+	echo ''
+	
+	exit 1
+fi
+
 BKP_FILE=$1
-BKP_FOLDER=$(echo $BKP_FILE | sed 's/\..*//')
-CONTAINER_NAME="config-db"
-NETWORK="config-service"
+CONTAINER_NAME=$2
+NETWORK=$3
+
+BKP_FOLDER=$(basename $BKP_FILE | sed 's/\..*//')
 WRK_DIR=/tmp/$CONTAINER_NAME
-BKP_DIR=/home/fac_files/backups/configdb
 
 mkdir -p $WRK_DIR
-tar -C $WRK_DIR -xvf $BKP_DIR/$1
-chmod -R +777 $BKP_DIR
+tar -C $WRK_DIR -xvf $BKP_FILE
+chmod -R +777 $BKP_FILE
 sudo docker run --rm --network $NETWORK --link $CONTAINER_NAME:mongo -v $WRK_DIR/tmp/$BKP_FOLDER:/backup mongo mongorestore --drop -v --host $CONTAINER_NAME /backup
 rm -rf $WRK_DIR
+
+exit 0
+
